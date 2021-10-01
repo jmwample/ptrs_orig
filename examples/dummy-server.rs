a0cfb4c02be4df5b17b65cdddd3285eb257c8201
jmwample <jack.wampler@colorado.edu>
2021-10-01 17:26:43 -0600

    Working through implementation and library skeleton
    Implementing Args strucutres to parse client parameters and server
    options. At the same time laying out skeleton for other desirable features
    like ffi, documentation, error handling, and example programs.
    This PR also adds a macro with tests for hashmap creation.
    All tests for args implementation passing.

--- examples/dummy-server.rs
+++ examples/dummy-server.rs
@@ -1,5 +1,57 @@
 
 
+use std::io;
+use std::thread;
+use std::time;
+use std::env;
+
+
+const NTHREADS: u32 = 10;
+
+// This is the `main` thread
 fn main() {
-    println!("hello world from dummy server");
+    // Make a vector to hold the children which are spawned.
+    let mut children = vec![];
+
+	if env::var("TOR_PT_EXIT_ON_STDIN_CLOSE") == Ok(String::from("1")) {
+		children.push(
+			thread::spawn( move || {
+				let mut buffer = String::new();
+				let stdin = io::stdin(); // We get `Stdin` here.
+				while stdin.read_line(&mut buffer).unwrap() != 0 {
+					buffer.clear();
+				}
+			})
+		);
+	}
+
+    for i in 0..NTHREADS {
+        // Spin up another thread
+        children.push(thread::spawn(move || {
+            println!("this is thread number {}", i);
+			thread::sleep(time::Duration::from_secs(3));
+        }));
+    }
+
+    for child in children {
+        // Wait for the thread to finish. Returns a result.
+        let _ = child.join();
+    }
 }
+
+
+// fn main() -> io::Result<()> {
+
+// 	if env::var()
+// 	let handle = thread::spawn( move || {
+// 		let mut buffer = String::new();
+// 		let stdin = io::stdin(); // We get `Stdin` here.
+// 		while stdin.read_line(&mut buffer).unwrap() != 0 {
+// 			buffer.clear();
+// 		}
+// 	});
+
+// 	handle.join().unwrap();
+
+// 	Ok(())
+// }
--- src/pt.rs
+++ src/pt.rs
@@ -1,12 +1,11 @@
 //! Pluggable Transports
 
-use url::Url;
 use std::env;
+use url::Url;
 
 use crate::error::PTError;
 
 const TOR_PT_PROXY: &str = "TOR_PT_PROXY";
-
 
 fn emit(keyword: &str, v: &[&str]) {
 	let mut vv = String::new();
@@ -18,12 +17,12 @@ fn emit(keyword: &str, v: &[&str]) {
 	println!("{}{}", keyword, vv);
 }
 
-
 // This structure is returned by [`client_setup`]. It consists of a list of method
 // names and the upstream proxy URL, if any.
+#[derive(Debug, Clone)]
 pub struct ClientInfo {
 	pub method_names: Vec<String>,
-	pub proxy_url:Url,
+	pub proxy_url: Option<Url>,
 }
 
 /// Check the client pluggable transports environment, emitting an error message
@@ -68,44 +67,44 @@ fn get_client_transports() -> Result<Vec<String>, Box<dyn std::error::Error>> {
 /// schemes; that is, one of "http", "socks5", or "socks4a". The caller must be
 /// able to handle any returned scheme (which may be by calling ProxyError if
 /// it doesn't know how to handle the scheme).
-fn get_proxy_url() -> Result<Url, Box<dyn std::error::Error>> {
+fn get_proxy_url() -> Result<Option<Url>, Box<dyn std::error::Error>> {
 	let tor_pt_proxy = match env::var(TOR_PT_PROXY) {
 		Ok(url) => url,
 		Err(err) => match err {
-			env::VarError::NotPresent => return Err(Box::new(PTError::NoProxyRequested)),
+			env::VarError::NotPresent => return Ok(None),
 			_ => return Err(Box::new(err)),
-		}
+		},
 	};
 
 	let url = Url::parse(&tor_pt_proxy)?;
 
 	if url.scheme() == "" {
-		return Err(Box::new(PTError::ProxyError(String::from("missing scheme"))))
+		return Err(Box::new(PTError::ProxyError(String::from(
+			"missing scheme",
+		))));
 	}
 
 	if !url.has_authority() {
-		return Err(Box::new(PTError::ProxyError(String::from("missing authority"))))
+		return Err(Box::new(PTError::ProxyError(String::from(
+			"missing authority",
+		))));
 	}
 
 	if !url.has_host() {
-		return Err(Box::new(PTError::ProxyError(String::from("missing host"))))
+		return Err(Box::new(PTError::ProxyError(String::from("missing host"))));
 	}
 
 	if url.port().is_none() {
-		return Err(Box::new(PTError::ProxyError(String::from("missing port"))))
+		return Err(Box::new(PTError::ProxyError(String::from("missing port"))));
 	}
 
-	Ok(url)
+	Ok(Some(url))
 }
 
-
-
-pub struct ServerInfo {
-}
-
+pub struct ServerInfo {}
 
 pub fn server_setup() -> Result<ServerInfo, Box<dyn std::error::Error>> {
-	Ok(ServerInfo{})
+	Ok(ServerInfo {})
 }
 
 #[cfg(test)]
@@ -114,8 +113,6 @@ mod tests {
 
 	#[test]
 	fn proxy_url_parse() {
-
-
 		let bad_cases: Vec<&str> = vec![
 			"bogus",
 			"http:",
@@ -154,32 +151,45 @@ mod tests {
 			("http://localhost:8080/", "http://localhost:8080/"),
 			("http://localhost:8080/path", "http://localhost:8080/path"),
 			("http://user@localhost:8080", "http://user@localhost:8080/"),
-			("http://user:password@localhost:8080", "http://user:password@localhost:8080/"),
+			(
+				"http://user:password@localhost:8080",
+				"http://user:password@localhost:8080/",
+			),
 			("socks5://localhost:1080", "socks5://localhost:1080"),
 			("socks4a://localhost:1080", "socks4a://localhost:1080"),
-			("unknown://localhost:9999/whatever", "unknown://localhost:9999/whatever"),
+			(
+				"unknown://localhost:9999/whatever",
+				"unknown://localhost:9999/whatever",
+			),
 		];
 
 		env::remove_var(TOR_PT_PROXY);
 		match get_proxy_url() {
-			Ok(url) => {
-				panic!("empty environment returned {}", url);
+			Ok(url) => match url {
+				Some(u) => panic!("empty environment returned {:?}", u),
+				None => {}
 			},
 			Err(err) => {
-				assert_eq!(err.to_string(), PTError::NoProxyRequested.to_string(), "empty environment returned unexpected error: {}", err);
-
-			},
+				panic!("empty environment returned unexpected error: {}", err);
+			}
 		}
 
 		for (input, expected) in good_cases {
 			env::set_var(TOR_PT_PROXY, input);
 			match get_proxy_url() {
 				Ok(u) => {
-					let url = String::from(u);
-					assert_eq!(&url, expected, "TOR_PT_PROXY={} → {} (expected {})", input, &url, expected);
+					let url = String::from(u.unwrap());
+					assert_eq!(
+						&url, expected,
+						"TOR_PT_PROXY={} → {} (expected {})",
+						input, &url, expected
+					);
 				}
 				Err(err) => {
-					panic!("TOR_PT_PROXY={} unexpectedly returned an error: {}", input, err);
+					panic!(
+						"TOR_PT_PROXY={} unexpectedly returned an error: {}",
+						input, err
+					);
 				}
 			}
 		}
@@ -188,12 +198,13 @@ mod tests {
 			env::set_var(TOR_PT_PROXY, input);
 			match get_proxy_url() {
 				Ok(url) => {
-					panic!("TOR_PT_PROXY={} unexpectedly succeeded and returned {:?}", input, url);
-				},
-				Err(_) => {},
+					panic!(
+						"TOR_PT_PROXY={} unexpectedly succeeded and returned {:?}",
+						input, url
+					);
+				}
+				Err(_) => {}
 			}
 		}
-
-
 	}
 }
